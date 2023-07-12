273ca611d0c17711586e1e68fbff0c1ee9ffce08
DavronMamaraimov <davronmamaraimov1996@gmail.com>
2023-07-12 10:35:52 +0500

    to be continued

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const textInput = document.getElementById('input-text');
 const buttonAdd = document.getElementById('btnAdd');
+const video = document.getElementById('main-box');
 
-const AddLink = () => {
-    console.log('True');
+const changeVideo = () => {
+    console.log(textInput.value);
+    video.innerHTML.src = './gallery/video2.mp4';
 }
 
