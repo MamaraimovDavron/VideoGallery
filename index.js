097736c9f3a42bb1da8eb5be1d4113f01cda047c
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-08-09 16:25:46 +0500

    in process

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-const textInput = document.getElementById('input-text');
-const buttonAdd = document.getElementById('btnAdd');
-const video = document.getElementById('main-box');
+const inputValue = document.getElementById("input");
+const videoBox = document.getElementById("box");
 
-const changeVideo = () => {
-    console.log(textInput.value);
-    video.innerHTML.src = './gallery/video2.mp4';
+
+
+const writeLink = () => {
+
+    // let initialPart;
+    // let middlePart;
+    // let lastPart;
+    // let newValue;
+
+    // if(inputValue.value == "") return;
+
+    if(inputValue.value.replace("watch?v","embed/")){
+        console.log(inputValue.value.replace("watch?v","embed/"));
+        // // console.log(inputValue.value.slice(24,31));
+        // let initialPart = inputValue.value.slice(0,24);
+        // // console.log(initialPart);
+
+        // let middlePart = inputValue.value.slice(24,31);
+        // middlePart = "embed/";
+        // // console.log(middlePart);
+
+        // let lastPart = inputValue.value.slice(31);
+        // // console.log(lastPart);
+
+        // let newValue = initialPart + middlePart + lastPart;
+        // // console.log(newValue);
+
+    }
+    // else if(inputValue.value.replace("watch?v","embed/"))
+
+    videoBox.innerHTML += `
+        <div class="col-lg-3 col-sm-1 col-md-4">
+            <iframe height="315" 
+                src=${inputValue.value.replace("watch?v","embed/")}
+                title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+    `;
 }
 
+// https://www.youtube.com/watch?v=4mvmzsp0QhA
