7bed9fed57f33f008ef13249336837472ecb2628
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-08-11 11:44:40 +0500

    Delete button added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inputValue = document.getElementById("input");
 const videoBox = document.getElementById("box");
 
-let index = 1;
+let index = 2;
 
 const writeLink = () => {
 
@@ -10,7 +10,7 @@ const writeLink = () => {
     // let lastPart;
     // let newValue;
 
-    // if(inputValue.value == "") return;
+    if(inputValue.value == "") return;
 
     // let changedValue = '';
 
@@ -46,23 +46,34 @@ const writeLink = () => {
 
     const col = document.createElement('div');
     col.className = "col col-lg-4 col-sm-1 col-md-4";
+    col.id = index;
+    console.log(index);
+    col.innerHTML = `
+        <button class="btn btn-danger" 
+            onclick="removeVideo('${index++}')">
+                <i class="bi bi-trash2-fill"></i>
+        </button>
 
-    const iframeBox = document.createElement('div');
-    iframeBox.className = "iframeBox";
-    iframeBox.src = inputValue.value.replace("watch?v","embed/");
+    `
+
+    const iframe = document.createElement('iframe');
+    iframe.className = "iframeBox";
+    iframe.src = inputValue.value.replace("watch?v=","embed/");
     // console.log(changedValue);
-    console.log(iframeBox.src)
+    // console.log(iframe.src)
 
-    col.appendChild(iframeBox);
+    col.appendChild(iframe);
     videoBox.appendChild(col);
+    // console.log(videoBox);
+    // console.log(col)
 
 }
 
 
 // https://www.youtube.com/watch?v=4mvmzsp0QhA
 
-const removeVideo = (index) => {
-    const element = document.getElementById(index);
+const removeVideo = (id) => {
+    const element = document.getElementById(id);
     element.remove();
-    console.log(index);
+    // console.log(index);
 }
